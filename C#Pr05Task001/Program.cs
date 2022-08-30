e7433cb3d663722ead4f81a4ec6d8d971ce6d4af
lyudmila <kudinova@alio-soft.ru>
2022-08-30 22:47:35 +0400

    Найти количество четных элементов массива

---
+++ C#Pr05Task001/Program.cs
@@ -0,0 +1,37 @@
+﻿// Задача 34: Задайте массив заполненный случайными положительными трёхзначными числами. Напишите программу, которая покажет количество чётных чисел в массиве.
+// [345, 897, 568, 234] -> 2
+
+int[] CreateArray( int length, int min, int max)
+{
+    int[] array = new int [length];
+    for(int i = 0; i<length; i++)
+    {
+        array[i] = new Random().Next(min, max+1);
+        if (i>=100) Console.Write(", ");
+    }
+    return array;
+}  
+
+void PrintArray(int[] array)
+{
+    for (int i = 0; i < array.Length; i++)
+        
+        Console.Write($"{array[i]} ");
+   
+   Console.WriteLine(); 
+}
+
+int FindEvenNumArray(int[] arr)
+{
+    int Length = 0;
+ 
+    for (int i = 0; i < arr.Length; i++)
+        if (arr[i] % 2 == 0)
+            Length++;
+ 
+    return Length;
+}
+
+int[] array = CreateArray(100, 100, 999);
+PrintArray(array);
+Console.WriteLine($"Количество четных элементов массива равно {FindEvenNumArray(array)}");
