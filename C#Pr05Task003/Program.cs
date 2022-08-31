52d1714f77f32d5930ad18cb9b8a8f2886879408
lyudmila <kudinova@alio-soft.ru>
2022-08-31 21:39:57 +0400

    Диф макс и мин элементов массива

---
+++ C#Pr05Task003/Program.cs
@@ -0,0 +1,41 @@
+﻿// Задача 38: Задайте массив вещественных чисел. 
+// Найдите разницу между максимальным и минимальным элементов массива.
+double[] CreateArray(int length, int min, int max)  // создает массив, заполняет его рандомно
+{
+    double[] array = new double[length];
+    for (int i = 0; i < length; i++)
+    {
+        array[i] = new Random().NextDouble()*(max - min) + min;
+        array[i] = Math.Round(array[i], 1);
+    }
+    return array;
+}
+
+double FindDifference(double[] array) //  сравнивает максимальное значение с минимальным
+{
+    double min = array[0];
+    double max = array[0];
+    for (int i = 0; i < array.Length; i++)
+    {
+        if (array[i] < min) min = array[i];
+        if (array[i] > max) max = array[i];
+    }
+    double difference = max - min;
+    return difference;
+}
+
+void PrintArray(double[] array) // выводит массив на консоль
+{
+    for (int i = 0; i < array.Length; i++)
+        
+        Console.Write($"{array[i]} ");
+   
+   Console.WriteLine();    
+}
+
+
+
+double[] array = CreateArray(15, -100, 100);
+PrintArray(array);
+double difference = FindDifference(array);
+Console.WriteLine($"Разница между максимальным и минимальным элементами массива: {difference}");
