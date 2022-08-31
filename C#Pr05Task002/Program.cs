41b6797f1bf6fb699f747c159ccda6793b940e5b
lyudmila <kudinova@alio-soft.ru>
2022-08-31 21:38:14 +0400

    Сумма нечетных индексов массива

---
+++ C#Pr05Task002/Program.cs
@@ -0,0 +1,39 @@
+﻿// // Задача 36: Задайте одномерный массив, заполненный случайными числами. 
+// // Найдите сумму элементов, стоящих на нечётных позициях.
+
+int[] CreateArray(int length, int min, int max) //создание массива
+
+{
+    int[] array = new int[length];
+    for (int i = 0; i < length; i++)
+    {
+        array[i] = new Random().Next(min, max + 1);
+    }
+    return array;
+}
+
+void PrintArray(int[] array) // вывод массива на консоль
+{
+    for (int i = 0; i < array.Length; i++)
+
+        Console.Write($"{array[i]} ");
+    Console.WriteLine();
+
+}
+
+int FindSum(int[] array)
+{
+    int sum = 0;
+    for (int i = 1; i < array.Length; i += 2)
+    {
+        sum += array[i];
+    
+    }
+    Console.WriteLine("сумма элементов, стоящих на нечётных позициях =" + sum);
+    return sum;
+}
+
+int[] array = CreateArray(15, -100, 100);
+PrintArray(array);
+FindSum(array);
+
